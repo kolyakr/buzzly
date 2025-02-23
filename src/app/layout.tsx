479c08cf68e09887450286d8fe2a87d3931f1e5a
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2025-02-23 12:05:59 +0200

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto ">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">sidebar</div>
+
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
